0c0b318fbed108012b468f65eccebf94ddb6429f
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-05-04 20:45:04 -0500

    Added a apidocs index.html file and setup the django admin site.

--- bangazonapi/admin.py
+++ bangazonapi/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from bangazonapi.models import Customer, Favorite, Order, OrderProduct, Payment, Product, ProductCategory, ProductRating, Rating, Recommendation
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Favorite)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
+admin.site.register(Payment)
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(ProductRating)
+admin.site.register(Rating)
+admin.site.register(Recommendation)
