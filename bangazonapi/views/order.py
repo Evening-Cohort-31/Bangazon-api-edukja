7c850a86aa1ca760639d4ecd45106fc89a5720e1
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-05-18 12:29:03 -0500

    Added null filter so only completed orders are returned

--- bangazonapi/views/order.py
+++ bangazonapi/views/order.py
@@ -140,7 +140,7 @@ class Orders(ViewSet):
             ]
         """
         customer = Customer.objects.get(user=request.auth.user)
-        orders = Order.objects.filter(customer=customer)
+        orders = Order.objects.filter(customer=customer, payment_type__isnull=False)
 
         payment = self.request.query_params.get('payment_id', None)
         if payment is not None:
