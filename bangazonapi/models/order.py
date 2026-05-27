dc6009dc4007b5a783f04028687852acdaf78e9a
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-05-26 23:00:53 -0500

    Updated Order created_date to default to today's actual date instead of 0000-00-00

--- bangazonapi/models/order.py
+++ bangazonapi/models/order.py
@@ -2,9 +2,10 @@
 from django.db import models
 from .customer import Customer
 from .payment import Payment
+import datetime
 
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING,)
     payment_type = models.ForeignKey(Payment, on_delete=models.DO_NOTHING, null=True)
-    created_date = models.DateField(default="0000-00-00",)
+    created_date = models.DateField(default=datetime.date.today)
